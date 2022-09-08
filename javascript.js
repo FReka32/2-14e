e48620c0f8e234f25d3faedda1cd04c82e99970c
Fazekas Réka <fazekasr@kkszki.hu>
2022-09-08 19:13:45 +0200

    js update

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var a = 2;
+var b = 3;
+switch(a){
+    case 1:
+        a=a+1;
+        console.log(a);
+    case 2: 
+        a=a-1;
+        console.log(a);
+    default:
+        a=3;
+        console.log(a);
+
+}
