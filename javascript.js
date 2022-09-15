0ef4ff72d66eca9f822d3574605408299a3f4d23
Fazekas Réka <fazekasr@kkszki.hu>
2022-09-15 18:43:14 +0200

    09.15 típusok, típus konverziók, ciklusok, lokális/globális deklarálás

--- javascript.js
+++ javascript.js
@@ -1,14 +1,38 @@
-var a = 2;
-var b = 3;
-switch(a){
-    case 1:
-        a=a+1;
-        console.log(a);
-    case 2: 
-        a=a-1;
-        console.log(a);
-    default:
-        a=3;
-        console.log(a);
+// Tipusok, tipuskonverziok
 
-}
+let value = true; // boolean - igaz / boolean - hamis - false
+console.log(typeof value);
+value = String(value); // tipus-kenyszerites
+console.log(typeof value);
+console.log("6"/"2"); // automata konvertalas szamma - / * -
+console.log("6"+"2"); // str concat
+
+let str = "123";
+console.log(typeof str); // string lesz alapertelmezve
+let num = Number(str);
+console.log(typeof num); // tipuskenyszerites szamma
+num = num + 1.2;
+console.log(num); // 124.2
+console.log(typeof num); // ez is csak siman Number
+
+let age = Number("tizennyolc"); //  
+console.log(age); // number tipusu, de NaN
+// Numerikus atalakitas lehetseges eredmenyei 
+// undefined - nem ertelmezheto a feladat -> Nan
+// null -> 0
+// true v. false (igaz-hamis) -> 1 es 0
+// string -> elveszi a whitespace karaktereket (\t, \n, stb)
+// ures string -> 0
+
+console.log(Number("   123   ")); // 123
+console.log(Number("123z")); // Nan
+console.log(Number(true)); // 1
+console.log(Number(false)); // 0
+
+// Boolean-konverzio
+console.log(Boolean(1)); // true
+console.log(Boolean(0)); // false
+console.log(Boolean("Hello")); // true
+console.log(Boolean("")); // false
+console.log(Boolean("0")); // true
+console.log(Boolean(" ")); // true
---
+++ javascript2.js
@@ -0,0 +1,38 @@
+/*let i = 0; 
+while (i < 3)
+{
+    console.log(i);
+    i++;
+}*/
+/*let i = 3;
+while (i) // i == true / i != 0;
+{
+    console.log(i);
+    i--;
+}
+*/
+/*let i = 0;
+do 
+{
+    console.log(i); // 0, 1, 2
+    i++; 
+} while (i < 3);
+*/
+/*
+let i = 3;
+do 
+{
+    console.log(i); // 3
+    i++; 
+} while (i < 3);*/
+//let i = 0; // globalis deklaarcio+ ertekadas
+//"use strict"
+//let i; // globalis deklaracio
+//i=5; //nem kell a let, értékefeluliras
+//for ( i=0; i < 3; i++) //'let' nelkul strict modban hiba, undefined; alapbol enged kulcsszo nelkul deklaralni
+//{
+//    console.log("lokalis:"+i);
+//    document.write("hello<br>");
+//}
+//let i=2; 
+//console.log("globalis:" + i); // a ciklusban levo i-t nem latja, hiba (alap/strict)
